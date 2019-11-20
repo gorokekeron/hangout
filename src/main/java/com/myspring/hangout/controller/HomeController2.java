3e7953c7a410097a4362ac027448f8691f968e23
SIST <SIST@DESKTOP-Q7IEVAP>
2019-11-20 18:55:54 +0900

    test

--- src/main/java/com/myspring/hangout/controller/HomeController2.java
+++ src/main/java/com/myspring/hangout/controller/HomeController2.java
@@ -24,7 +24,7 @@ public class HomeController2 {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome home222222! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
